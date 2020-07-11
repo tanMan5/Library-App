f98e4f5bdde21b300ebc3cc590c712772d10b50c
Sumnima B <sumnimabhandari@gmail.com>
2020-07-11 13:32:37 -0700

    Fixed linting

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
+//const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.render("signup",{});
+    res.render("signup", {});
   });
 
   app.get("/login", (req, res) => {
@@ -24,6 +24,6 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.render("members",{});
+    res.render("members", {});
   });
 };
