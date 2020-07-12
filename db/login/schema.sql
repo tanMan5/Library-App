4fb98d38b23ec0435e973a019345a826b9c0779f
Tanner Nielsen <tan-ali5@outlook.com>
2020-07-11 21:33:52 -0700

    ratings and books db

--- db/login/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE userlogin_db;
-USE userlogin_db;
-
-CREATE TABLE login (
-	id int NOT NULL AUTO_INCREMENT,
-	email varchar(255) NOT NULL,
-	password varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
