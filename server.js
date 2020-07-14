c88cb3ec3d64032efa3c360f0967b7c0ee5ccb79
Sumnima B <sumnimabhandari@gmail.com>
2020-07-13 19:05:48 -0700

    Added .env file with database info

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Requiring necessary npm packages
+require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
