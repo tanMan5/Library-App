26b57d75fd8d04fd3daf4fdff277f915b6b5326e
Tanner Nielsen <tan-ali5@outlook.com>
2020-07-13 21:26:32 -0700

    script added to member

--- public/js/googlebook.js
+++ public/js/googlebook.js
@@ -1,15 +1,13 @@
-
 function getBooks() {
- 
   const APIKey = "AIzaSyA6IRz2PfBZ2HyZlTjr0QD9_nZqZctgFKg";
 
-  const queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + "&key=" + APIKey;
+  const queryURL =
+    "https://www.googleapis.com/books/v1/volumes?q=" + "&key=" + APIKey;
 
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
-    
+  }).then(response => {
     console.log(response);
     console.log(response);
   });
