e4c1d008893780d79a5c80c6a16eda37cdd292ce
Sumnima B <sumnimabhandari@gmail.com>
2020-07-13 21:32:08 -0700

    Updated googlebook.js with google book api info

--- public/js/googlebook.js
+++ public/js/googlebook.js
@@ -1,16 +1,19 @@
-
 function getBooks() {
- 
   const APIKey = "AIzaSyA6IRz2PfBZ2HyZlTjr0QD9_nZqZctgFKg";
-
-  const queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + "&key=" + APIKey;
-
+  const searchedBook = $(".input").val();
+  const queryURL =
+    "https://www.googleapis.com/books/v1/volumes?q=intitle:" + searchedBook + "&key=" + APIKey;
+    // "https://www.googleapis.com/books/v1/volumes?q=0262527359";
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
-    
-    console.log(response);
+  }).then(response => {
     console.log(response);
   });
 }
+
+// Book search button
+$(".searchButton").on("click", function (e) {
+  e.preventDefault();
+  getBooks();
+})
